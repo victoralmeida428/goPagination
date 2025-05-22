ee3293924d4d0ce599fd7814c318596a72625ccd
victorgomes <victor.almeida@unigranrio.br>
2025-05-22 13:07:30 -0300

    correcao validacao

--- pagination/validation.go
+++ pagination/validation.go
@@ -9,7 +9,7 @@ func (p PaginationAbstract[E]) validate() error {
 	if p.pageNum < 1 {
 		return errors.New("pagination pageNum cannot be less than 1")
 	}
-	if p.totalCount+p.pageSize <= p.pageSize*p.pageNum {
+	if p.totalCount > 0 && p.totalCount+p.pageSize <= p.pageSize*p.pageNum {
 		
 		return errors.New("pagination pageNum and totalCount must be greater than pageSize")
 	}
